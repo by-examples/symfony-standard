9086e8979b89ff5fc23c3fa6dfa2648a723d3826
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2013-01-05 12:59:58 +0100

    [STARTUP] StofDoctrineExtensionsBundle installation
    Conflicts:
    	app/AppKernel.php
    
    Conflicts:
    	app/AppKernel.php
    	app/config/config.yml
    	composer.json

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new My\UserBundle\MyUserBundle(),
+            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
