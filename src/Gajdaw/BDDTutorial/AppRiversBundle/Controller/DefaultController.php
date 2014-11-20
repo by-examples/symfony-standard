a973d7d7a8e85b392613209b8f897eb67e10852d
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2014-11-20 10:51:29 +0100

    Iteration 3.2 - Error pages
    Conflicts:
    	features/error.feature

--- src/Gajdaw/BDDTutorial/AppRiversBundle/Controller/DefaultController.php
+++ src/Gajdaw/BDDTutorial/AppRiversBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace Gajdaw\BDDTutorial\AppRiversBundle\Controller;
 
+use Gedmo\Exception\RuntimeException;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
@@ -15,5 +16,14 @@ class DefaultController extends Controller
     public function indexAction()
     {
         return array();
+    }
+
+    /**
+     * @Route("/action/with/exception")
+     * @Template()
+     */
+    public function actionWithExceptionAction()
+    {
+        throw new RuntimeException('Ups...');
     }
 }
