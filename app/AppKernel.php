80d402d4dd3cea9e46ffcf495b5aae7d53d14b60
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2014-11-20 10:40:01 +0100

    Iteration 3.1 - Homepage

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             new My\UserBundle\MyUserBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
+            new Gajdaw\BDDTutorial\AppRiversBundle\GajdawBDDTutorialAppRiversBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
