41bbf3a9946f5c9fbc9563a6c62078a671a84e09
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2014-11-28 16:46:34 +0100

    [STARTUP] JmsSerializerBundle installation
    Conflicts:
    	app/AppKernel.php
    
    Conflicts:
    	app/AppKernel.php
    	composer.json

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             new My\UserBundle\MyUserBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
