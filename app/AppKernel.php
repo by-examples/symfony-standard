2544a10f42e33dd3c88b966487e144d22614468c
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2015-08-29 09:43:23 +0200

    [STARTUP] FOSRestBundle
    Conflicts:
    	app/AppKernel.php
    	composer.json

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -25,6 +25,7 @@ class AppKernel extends Kernel
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
             new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
             new Braincrafted\Bundle\BootstrapBundle\BraincraftedBootstrapBundle(),
+            new FOS\RestBundle\FOSRestBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
